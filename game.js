a1a15118c54bf99056f89b3610fe0b6c15e9703f
Henry Hayes <hennus.hayes@gmail.com>
2020-09-16 10:19:30 -0700

    questions and answers fixed

--- game.js
+++ game.js
@@ -1,10 +1,11 @@
-const question = document.getElementById("question");
-const choices = Array.from(document.getElementsByClassName("choice-text"));
+const question = document.getElementById('question');
+const choices = Array.from(document.getElementsByClassName('choice-text'));
 
 let currentQuestion = {};
-let acceptingAnswers = false;//This is to have a delay
+let acceptingAnswers = false;
+let score = 0;
 let questionCounter = 0;
-let availableQuestions = [];//This signals an array
+let availableQuesions = [];
 
 let questions = [
     {
@@ -32,43 +33,48 @@ let questions = [
         choice4: "alert('Hello World');",
         answer: 4,
     },
-];//pulled straight from github repo. This is an array and within array there are objects with different fields namely, question, choices and the correct answer key
+];
 
-const Correct_Bonus = 10;//the user gets 10 points if they answer correctly
-const MAX_QUESTIONS = 3;//how many questions before they finish
+//CONSTANTS
+const CORRECT_BONUS = 10;
+const MAX_QUESTIONS = 3;
 
 startGame = () => {
     questionCounter = 0;
     score = 0;
-    availableQuestions = {...questions};//This basically copies the questions array
+    availableQuesions = [...questions];
     getNewQuestion();
 };
-//parameters go into the brackets if there are any.
+
 getNewQuestion = () => {
-    if(availableQuestions.length === 0 || questionCounter>= MAX_QUESTIONS){
-        return window.location.assign("/end.html");
+    if (availableQuesions.length === 0 || questionCounter >= MAX_QUESTIONS) {
+        //go to the end page
+        return window.location.assign('/end.html');
     }
     questionCounter++;
-    const questionIndex = Math.floor(Math.random()*availableQuestions.length);
-    currentQuestion = availableQuestions[questionIndex];
-    question.innerText = currentQuestion.question;//.question refers to the question field in the array of objects
-    
-    choices.forEach(choice => {
-        const number = choice.dataset["number"];
-        choice.innerText = currentQuestion["choice" + number];
+    const questionIndex = Math.floor(Math.random() * availableQuesions.length);
+    currentQuestion = availableQuesions[questionIndex];
+    question.innerText = currentQuestion.question;
 
+    choices.forEach((choice) => {
+        const number = choice.dataset['number'];
+        choice.innerText = currentQuestion['choice' + number];
     });
-    availableQuestions.splice(questionIndex, 1);
+
+    availableQuesions.splice(questionIndex, 1);
     acceptingAnswers = true;
 };
-choices.forEach(choice => {
-    choice.addEventListener("click", e => {
-        if(!acceptingAnswers) return;
+
+choices.forEach((choice) => {
+    choice.addEventListener('click', (e) => {
+        if (!acceptingAnswers) return;
 
         acceptingAnswers = false;
         const selectedChoice = e.target;
-        const selectedAnswer = selectedChoice.dataset["number"];
+        const selectedAnswer = selectedChoice.dataset['number'];
         getNewQuestion();
     });
 });
+
 startGame();
+
